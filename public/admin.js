36e38436e5f6f69c99d9c94f85b980758f36f7cc
Carlos Hernandez <chernandezmiranda@gmail.com>
2023-10-11 10:32:28 -0700

    added fetch, patch & buttons

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,24 @@
+const { application } = require("express");
 
 // Your Code Here
+async function getBooks() {
+    let response = await fetch('http://localhost:3001/listBooks')
+    let resultList = await response.json();
+    console.log(resultList)
+};
+
+
+async function changeTitle (){
+    let response = await fetch('http://localhost:3001/updateBook', {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            id:3,
+            title: 'The legends of Arathrae (updated)'
+        })
+    });
+    let json = await response.json();
+    console.log(json)
+}
