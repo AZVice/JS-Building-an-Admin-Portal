6c2a8180d8a49d7324ebdaae7ed2c20ba99b034c
Carlos Hernandez <chernandezmiranda@gmail.com>
2023-10-18 18:46:00 -0700

    incomplete

--- public/admin.js
+++ public/admin.js
@@ -1,13 +1,64 @@
-const { application } = require("express");
+/*const { application } = require("express");*/
 
 // Your Code Here
+// GET a list of books 
 async function getBooks() {
     let response = await fetch('http://localhost:3001/listBooks')
     let resultList = await response.json();
     console.log(resultList)
 };
 
+/*
+async function updateQuantity(inputId){
+    const quantityInput = document.getElementById(inputId);
+    const newQuantity = quantityInput.value;
 
+    let response = await fetch('http://localhost:3001/updateBook', {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            title:inputId.replace("Quantity", ""),
+            quantity: newQuantity
+        })
+    });
+    let json = await response.json();
+    console.log(json);
+}
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Updated book id-3 to the legends of Arathrae  (for Testing purposes only)
+/*
 async function changeTitle (){
     let response = await fetch('http://localhost:3001/updateBook', {
         method: 'PATCH',
@@ -16,9 +67,31 @@ async function changeTitle (){
         },
         body: JSON.stringify({
             id:3,
-            title: 'The legends of Arathrae (updated)'
+            title: 'The legends of Arathrae'
         })
     });
     let json = await response.json();
     console.log(json)
 }
+*/
+
+/*async function updateQuantity(title){
+    const quantityInput = document.getElementById(`${title}Quantity`);
+    const newQuantity = quantityInput.value;
+
+    let response = await fetch('http://localhost:3001/updateBook', {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            title: title,
+            quantity: newQuantity
+        })
+    });
+    let json = await response.json();
+    console.log(json);
+}
+*/
+
+
